d12e38a93c0506318fdb2cfdbc43791ac061e38e
jeevan <javaindustryexperience@gmail.com>
2023-09-27 20:15:20 +0530

    added the code for 1 to 5 number

---
+++ SpringBootDemo/src/com/velocity/TestExample.java
@@ -0,0 +1,11 @@
+package com.velocity;
+
+public class TestExample {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("Value of i is>>" + i);
+		}
+	}
+}
