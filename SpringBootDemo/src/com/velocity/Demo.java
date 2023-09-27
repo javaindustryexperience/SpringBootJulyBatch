9bff7493eb05d69d5d365dc47703e2e3b6988903
jeevan <javaindustryexperience@gmail.com>
2023-09-27 20:29:04 +0530

    added the arraylist code

---
+++ SpringBootDemo/src/com/velocity/Demo.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		List l = new ArrayList();
+		l.add(10);
+		l.add(20);
+		l.add(30);
+		System.out.println("In arrayList>>" + l);
+	}
+}
