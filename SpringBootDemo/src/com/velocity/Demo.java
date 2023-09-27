696725866d812ea5a2549d2565b7ecf88b4f1e7b
jeevan <javaindustryexperience@gmail.com>
2023-09-27 21:06:01 +0530

    changes in Demo.Java and use generics in the program

--- SpringBootDemo/src/com/velocity/Demo.java
+++ SpringBootDemo/src/com/velocity/Demo.java
@@ -7,10 +7,11 @@ public class Demo {
 
 	public static void main(String[] args) {
 
-		List l = new ArrayList();
-		l.add(10);
-		l.add(20);
-		l.add(30);
+		List<String>l = new ArrayList<String>();
+		l.add("10");
+		l.add("20");
+		l.add("30");
+		System.out.println("arraylist program");
 		System.out.println("In arrayList>>" + l);
 	}
 }
